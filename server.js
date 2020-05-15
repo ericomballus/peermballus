4dc77a53621d79aedf3ad436ab349c8a8a72e543
ericomballus <eric2mballus@gmail.com>
2020-05-15 07:38:57 +0100

    peerserver

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 //const { ExpressPeerServer } = require("peer");
-const port = process.env.PORT || 9000;
 
+const cors = require("cors");
+
+const port = process.env.PORT || 9000;
+let io = require("socket.io").listen(
+  app.listen(port, () => {
+    console.log(`listen in port ${port}`);
+    // console.log(process.env);
+  })
+);
+io.sockets.on("connection", function (socket) {
+  console.log("client connect=====erico test");
+  socket.on("echo", function (data) {
+    io.sockets.emit("message", data);
+  });
+});
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
